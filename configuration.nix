e37f0d3110718cec32d6feaf22703c5ae6ce78e2
Dylan Kirby <dylan.kirby.365@gmail.com>
2024-11-17 18:16:42 +0200

    Initial Commit

---
+++ configuration.nix
@@ -0,0 +1,56 @@
+{  lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "kirby";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Africa/Johannesburg";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
+  services.xserver.xkb.layout = "za";
+  services.printing.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  users.users.dylan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      firefox
+      htop
+      git
+      vim
+      tree
+      vscode
+      python3
+      jdk21
+    ];
+  };
+
+  
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # system.copySystemConfiguration = true;
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dylan Kirby's NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
