4f3fb5cc4434e0ec20b1e8567b62693dc25d5098
Dylan Kirby <dylan.kirby.365@gmail.com>
2024-11-18 15:35:05 +0200

    Add home manager basic install

---
+++ home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "dylan";
+  home.homeDirectory = "/home/dylan";
+
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+  ];
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
