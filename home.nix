ce4d7a685307df33efd808ae8d67c1364cedede8
Dylan Kirby <dylan.kirby.365@gmail.com>
2024-11-19 22:05:26 +0200

    Configure GNOME

--- home.nix
+++ home.nix
@@ -16,6 +16,8 @@
     pkgs.python3
     pkgs.jdk21
     pkgs.helix
+
+    pkgs.kitty
     # pkgs-unstable.example
   ];
 
@@ -118,6 +120,44 @@
           error_symbol = "[➜](bold red)";
         };
       };
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "firefox.desktop"
+        "vscode.desktop"
+      ];
+      had-bluetooth-devices-setup = true;
+      remember-mount-password = false;
+      welcome-dialog-last-shown-version = "42.4";
+    };
+    "org/gnome/shell/extensions/hidetopbar" = {
+      enable-active-window = false;
+      enable-intellihide = false;
+    };
+    "org/gnome/desktop/interface" = {
+      clock-show-seconds = true;
+      clock-show-weekday = true;
+      color-scheme = "prefer-dark";
+      enable-hot-corners = false;
+      font-antialiasing = "grayscale";
+      font-hinting = "slight";
+      gtk-theme = "Nordic";
+      toolkit-accessibility = true;
+    };
+    "org/gnome/mutter" = {
+      dynamic-workspaces = true;
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      name = "Open Console";
+      command = "kitty -e tmux";
+      binding = "<Ctrl><Alt>t";
     };
   };
 
