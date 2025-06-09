f7ae6becdc0c60d62a524f102a2f962c4eb88d1d
Dylan Kirby <25853805@sun.ac.za>
2025-06-09 09:53:21 +0200

    Update warper

---
+++ nix-shells/rust-shell.nix
@@ -0,0 +1,21 @@
+let
+  stable = import <nixpkgs> {};
+  unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+in
+
+with stable;
+
+mkShell {
+  nativeBuildInputs = [
+    pkg-config
+    openssl
+    gcc
+    rustup
+    man-pages
+    man-pages-posix
+  ];
+
+  shellHook = ''
+    export PATH=$PATH:$HOME/.cargo/bin
+  '';
+}
