74d3cc66c9d85b10747f78534f14da9e7302b6fd
Dylan Kirby <dylan.kirby.365@gmail.com>
2025-08-12 11:50:04 +0200

    jupyter shell

--- nix-shells/jupyter-shell.nix
+++ nix-shells/jupyter-shell.nix
@@ -1,39 +1,41 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-
 with pkgs;
-
 mkShell {
   nativeBuildInputs = [
-
     (python3.withPackages (
       ps: with ps; [
-
-        # Packages
+        # Core packages
         numpy
         pandas
         matplotlib
         opencv4
         scikit-image
 
-        # ipython
+        # Jupyter ecosystem
         jupyter
         ipython
         ipykernel
 
-        # LSP
+        # LSP and formatting
         python-lsp-server
         python-lsp-black
-        lsprotocol
         pyls-isort
+        pylsp-mypy
+        python-lsp-ruff
+
+        # JupyterLab extensions
         jupyterlab-lsp
+        jupyterlab-git
+        # jupyterlab-vim  # vim keybindings
       ]
     ))
+    nodejs
   ];
 
   shellHook = ''
-    jupyter lab
+    jupyter lab --ip=127.0.0.1 --port=8888
     exit
   '';
 }
