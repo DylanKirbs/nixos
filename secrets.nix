f3ef2f99e96a5ba20b5ff728e7df9ab2c9fc8d75
Dylan Kirby <25853805@sun.ac.za>
2026-03-30 17:27:25 +0200

    Regen with lab key

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,10 @@
 let
   lab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdjS7qrAzVG2go7ndHik2wljTpslMiJQkwAcE/pzwa5 root@nixos";
-  # TODO: Add home host key from /etc/ssh/ssh_host_ed25519_key.pub.
-  # home = "ssh-ed25519 AAAA... root@home";
-  # TODO: Add work host key from /etc/ssh/ssh_host_ed25519_key.pub.
-  # work = "ssh-ed25519 AAAA... root@work";
+  home = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIj7lWp0/L1l2qqRcSDN+HQvgkXLSXjmsQr3HcQQEm3C root@nixos";
 in
 {
   "secrets/zerotier-network-id.age".publicKeys = [
     lab
-    # home
-    # work
+    home
   ];
 }
